6b348b3735dfec6894decb9c91069a3da7993a56
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-28 11:11:26 +0100

    subscribe component structure

---
+++ reactinitial/src/components/Subscription.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const Subscription = () => {
+  const [email, setEmail] = useState("");
+
+  const subscribe = () => {
+
+  }
+
+  return (
+    <div>
+      <h2>Subscribe to our newsletter!!!!!!</h2>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <button onClick={subscribe}>Subscribe</button>
+    </div>
+  )
+}
+
+export default Subscription
