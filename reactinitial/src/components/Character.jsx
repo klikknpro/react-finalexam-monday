f30fd65d1ec5e23fcd7252da5c6dffd67e22a691
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-28 11:57:08 +0100

    add basic mui

--- reactinitial/src/components/Character.jsx
+++ reactinitial/src/components/Character.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
 
 const Character = ({character}) => {
   const [isShown, setIsShown] = useState(false);
@@ -11,7 +12,7 @@ const Character = ({character}) => {
     <div>
       <h2>{character.name}</h2>
       {isShown && <p>{character.details}</p>}
-      <button onClick={toggleDetails}>{isShown ? "Show less" : "Show more"}</button>
+      <Button variant="contained" onClick={toggleDetails}>{isShown ? "Show less" : "Show more"}</Button>
     </div>
   )
 }
