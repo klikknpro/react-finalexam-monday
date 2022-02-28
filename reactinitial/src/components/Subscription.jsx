1229681d4ada130a2b4e49b7b8cc5536c4a0ee80
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-28 11:41:39 +0100

    loading mask on subscribe

--- reactinitial/src/components/Subscription.jsx
+++ reactinitial/src/components/Subscription.jsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react'
 import http from "axios";
+import LoadingMask from './LoadingMask';
 
 const Subscription = () => {
   const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const subscribe = async() => {
+    setIsLoading(true);
     // {email} body is jo
     const response = await http.post("https://seriescharacters.com/api/newsletter", {
       email: email
     });
+    setIsLoading(false);
+
 
   }
 
   return (
     <div>
       <h2>Subscribe to our newsletter!!!!!!</h2>
-      <input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <button onClick={subscribe} disabled={!(email.includes("@") && email.includes("."))}>Subscribe</button>
+      {isLoading ? <LoadingMask/> :
+        <div>
+          <input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <button onClick={subscribe} disabled={!(email.includes("@") && email.includes("."))}>Subscribe</button>
+        </div>
+      }
     </div>
   )
 }
