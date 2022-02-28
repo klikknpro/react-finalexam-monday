e5eb47edc1c36ed3963b81f3c5eb74bfa5117595
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-28 11:03:53 +0100

    show details on click

--- reactinitial/src/components/Character.jsx
+++ reactinitial/src/components/Character.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Character = ({character}) => {
+  const [isShown, setIsShown] = useState(false);
+
+  const toggleDetails = () => {
+    setIsShown(!isShown);
+  }
+
   return (
-    <div>{character.name}</div>
+    <div>
+      <h2>{character.name}</h2>
+      <button onClick={toggleDetails}>Show more</button>
+      {isShown && <p>{character.details}</p>}
+    </div>
   )
 }
 
