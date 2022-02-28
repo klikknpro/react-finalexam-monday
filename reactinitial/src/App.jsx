cc258ebffabd93a7a2fcc4b63e7569cc238837c3
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-02-28 10:57:13 +0100

    character names are showing

--- reactinitial/src/App.jsx
+++ reactinitial/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import http from "axios";
 import LoadingMask from "./components/LoadingMask";
+import Character from "./components/Character";
 
 const App = () => {
   const [characters, setCharacters] = useState(null);
@@ -17,7 +18,11 @@ const App = () => {
   return (
     <div>
       <h1>Series API</h1>
-      {characters ? "characters loaded" : <LoadingMask/>}
+      {characters ?
+       characters.map((character, i) => (
+         <Character character={character} key={i}/>
+       ))
+       : <LoadingMask/>}
     </div>
   )
 }
---
+++ reactinitial/src/components/Character.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Character = ({character}) => {
+  return (
+    <div>{character.name}</div>
+  )
+}
+
+export default Character
